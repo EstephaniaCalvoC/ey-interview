0ab009061e5134281605086e86ecde47cf2dd4b9
Estephania Calvo <estephania.calvo.c@gmail.com>
2024-05-18 18:31:20 -0500

    Add error handler for vector db

--- back/vector_dbs.py
+++ back/vector_dbs.py
@@ -1,6 +1,7 @@
 import abc
 
-from back.models.embeddings import EmbeddingModel
+from models.embeddings import EmbeddingModel
+from utils.exceptions import ExternalException
 from configs import AzureAISearchConfig
 from langchain_community.vectorstores.azuresearch import AzureSearch
 
@@ -15,14 +16,25 @@ class VectorDB(metaclass=abc.ABCMeta):
 class AzureAISearchVectorDB():
     
     def __init__(self, embeddings: EmbeddingModel) -> None:
-        self.__vector_store = AzureSearch(
-            azure_search_endpoint= AzureAISearchConfig.endpoint,
-            azure_search_key=AzureAISearchConfig.key,
-            index_name=AzureAISearchConfig.index_name,
-            embedding_function=embeddings.model.embed_query
-            )    
+        self.embeddings = embeddings
+        self.__vector_store = self.__get_vector_store() 
 
     
     def embed_documents(self, documents) -> None:
-        self.__vector_store.add_documents(documents=documents) 
+        try:
+            self.__vector_store.add_documents(documents=documents) 
+        except Exception as e:
+            raise ExternalException("There was an error connecting to Azure Search", e)
+    
+    
+    def __get_vector_store(self) -> AzureSearch:
+        try:
+            return AzureSearch(
+            azure_search_endpoint= AzureAISearchConfig.endpoint,
+            azure_search_key=AzureAISearchConfig.key,
+            index_name=AzureAISearchConfig.index_name,
+            embedding_function=self.embeddings.model.embed_query
+            )  
+        except Exception as e:
+            raise ExternalException("There was an error connecting to Azure Search", e)
         
