6b6d739be7aa98b83f7f8f819b70e5f0093fb6a1
Estephania Calvo <estephania.calvo.c@gmail.com>
2024-05-24 18:02:28 -0500

    Fix format in client

--- client.py
+++ client.py
@@ -8,21 +8,19 @@ if __name__ == "__main__":
     #     json={'input': {"input": "How is Mercedez-Benz using AI?"}}
     # )
     # print(response.json()["output"])
-    
+
     response = requests.post(
-        "http://localhost:8000/rag/invoke",
-        json={'input': {"answer": "How is Mercedez-Benz using AI?"}}
+        "http://localhost:8000/rag/invoke", json={"input": {"answer": "How is Mercedez-Benz using AI?"}}
     )
     print(response.text)
-    
+
     # response = requests.post(
     #     "http://localhost:8000/rag/invoke",
     #     json={'input': "How is Mercedez-Benz using AI?"}
     # )
     # print(response.text)
-    
+
     # response = requests.get(
     #     "http://localhost:8000/rag/input_schema"
     # )
     # print(response.text)
-    
