b1b515c5e443a9b342cd144644ca4a9c75dcbfa8
Estephania Calvo <estephania.calvo.c@gmail.com>
2024-05-24 17:58:42 -0500

    Fix format in front file

--- front.py
+++ front.py
@@ -1,4 +1,3 @@
-import time
 import streamlit as st
 from langserve import RemoteRunnable
 
@@ -8,13 +7,13 @@ st.header("EY Chatbot Demo")
 
 
 if "rag_chain" not in st.session_state:
-    st.session_state.rag_chain = RemoteRunnable("http://localhost:8000/rag")   
-    
-    
+    st.session_state.rag_chain = RemoteRunnable("http://localhost:8000/rag")
+
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
-   
-    
+
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
@@ -22,13 +21,12 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What can I help you with?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    
+
     with st.chat_message("user"):
         st.markdown(prompt)
-        
+
     with st.chat_message("assistant"):
         response = st.session_state.rag_chain.invoke({"input": prompt})
         st.markdown(response)
-        
+
     st.session_state.messages.append({"role": "assistant", "content": response})
-        
