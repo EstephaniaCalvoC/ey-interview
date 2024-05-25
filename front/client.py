451fd8e773dd250ca54902c2773478c4066c0b41
Estephania Calvo <estephania.calvo.c@gmail.com>
2024-05-25 15:25:38 -0500

    Test linter - Separte invoke_chain_without_input_field

--- front/client.py
+++ front/client.py
@@ -8,14 +8,18 @@ def invoke_chain():
     return response.json()["output"]
 
 
+def invoke_chain_without_input_field():
+    response = requests.post(
+        "http://localhost:8000/rag/invoke", json={"input": {"answer": "How is Mercedez-Benz using AI?"}}
+    )
+    return response.text
+
+
 if __name__ == "__main__":
 
     # print(invoke_chain())
 
-    response = requests.post(
-        "http://localhost:8000/rag/invoke", json={"input": {"answer": "How is Mercedez-Benz using AI?"}}
-    )
-    print(response.text)
+    print(invoke_chain_without_input_field())
 
     # response = requests.post(
     #     "http://localhost:8000/rag/invoke",
